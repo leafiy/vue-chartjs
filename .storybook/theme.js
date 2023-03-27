ae2784c8e9869b0242f6afae3a7f45e2d9b19c6d
leafiy <tmly2006@gmail.com>
2023-03-28 02:01:52 +0800

    update

--- .storybook/theme.js
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming'
-
-export const theme = create({
-  base: 'light',
-  brandTitle: 'vue-chartjs',
-  brandUrl: 'https://github.com/apertureless/vue-chartjs'
-})
